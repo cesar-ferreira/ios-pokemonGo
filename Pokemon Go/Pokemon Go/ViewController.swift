601123cdb482d4c6215e0e04e5bd2ff46db9790e
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-01-07 17:55:09 -0300

    random anotation

--- Pokemon Go/Pokemon Go/ViewController.swift
+++ Pokemon Go/Pokemon Go/ViewController.swift
@@ -23,32 +23,14 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         managerLocation.requestWhenInUseAuthorization()
         managerLocation.startUpdatingLocation()
+        
+        showPokemon()
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     
         if (count < 5) {
-            
-            if let coordenate = managerLocation.location?.coordinate {
-                let region = MKCoordinateRegion(center: coordenate, latitudinalMeters: 200, longitudinalMeters: 200)
-                
-                map.setRegion(region, animated: true)
-                
-                /*
-                let userLocation: CLLocation = locations.last!
-                
-                let latitude: CLLocationDegrees = (userLocation.coordinate.latitude)
-                let longitude: CLLocationDegrees = (userLocation.coordinate.longitude)
-                
-                let deltaLat: CLLocationDegrees = 0.01
-                let deltaLog: CLLocationDegrees = 0.01
-                
-                let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-                let area: MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: deltaLat, longitudeDelta: deltaLog)
-                let region: MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: area)
-                map.setRegion(region, animated: true)
-                */
-            }
+            centralizePlayer()
             
             count += 1
         } else {
@@ -83,6 +65,55 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         }
         
     }
+    
+    private func showPokemon() {
+        Timer.scheduledTimer(withTimeInterval: 8, repeats: true) { (timer) in
+             
+            if let coordenate = self.managerLocation.location?.coordinate {
+                let anotation = MKPointAnnotation()
+                
+                let randomLat = ( Double( arc4random_uniform(400) ) - 200 ) / 100000.0
+                let randomLon = ( Double( arc4random_uniform(400) ) - 200 ) / 100000.0
 
+                anotation.coordinate = coordenate
+                anotation.coordinate.latitude += randomLat
+                anotation.coordinate.longitude += randomLon
+                
+                self.map.addAnnotation( anotation )
+            }
+        }
+    }
+    
+    private func centralizePlayer() {
+        if let coordenate = managerLocation.location?.coordinate {
+            let region = MKCoordinateRegion(center: coordenate, latitudinalMeters: 200, longitudinalMeters: 200)
+            
+            map.setRegion(region, animated: true)
+            
+            /*
+            let userLocation: CLLocation = locations.last!
+            
+            let latitude: CLLocationDegrees = (userLocation.coordinate.latitude)
+            let longitude: CLLocationDegrees = (userLocation.coordinate.longitude)
+            
+            let deltaLat: CLLocationDegrees = 0.01
+            let deltaLog: CLLocationDegrees = 0.01
+            
+            let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+            let area: MKCoordinateSpan = MKCoordinateSpan.init(latitudeDelta: deltaLat, longitudeDelta: deltaLog)
+            let region: MKCoordinateRegion = MKCoordinateRegion.init(center: location, span: area)
+            map.setRegion(region, animated: true)
+            */
+        }
+    }
+
+    @IBAction func centralize(_ sender: Any) {
+        centralizePlayer()
+    }
+    
+    @IBAction func openPokedex(_ sender: Any) {
+        
+    }
+    
 }
 
